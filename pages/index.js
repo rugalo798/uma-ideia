39719a73750446bd4a6272c5f85403814f983a06
rugalo798 <tiagovinicius79810@gmail.com>
2021-05-03 23:29:02 -0300

    testando Link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
-const Home = () => {
-    return <h1>Home</h1>
-}
+import Link from 'next'
 
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Link href="/sobre">
+                <a>Acessar a página Sobre!</a>
+            </Link>
+        </div>   
+    )
+}
 
 export default Home;
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next'
+
+const Sobre = () => {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <Link href="/">
+                <a>Acessar a página Home!</a>
+            </Link>
+        </div>   
+    )
+}
+
+export default Sobre;

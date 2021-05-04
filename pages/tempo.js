8c69a12b5900bd2c89f3162424f3992be7871136
rugalo798 <tiagovinicius79810@gmail.com>
2021-05-04 00:02:11 -0300

    Adicionando logs

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,16 +1,18 @@
 const Tempo = (props) => {
+  console.log("Passando pelo Frontend");
   const dynamicDate = new Date();
   const dynamicDateString = dynamicDate.toGMTString();
 
   return (
-      <div>
-          <div>{dynamicDateString} (dinâmico)</div>
-          <div>{props.staticDateString} (estatico)</div>
-      </div>
-  )
+    <div>
+      <div>{dynamicDateString} (dinâmico)</div>
+      <div>{props.staticDateString} (estatico)</div>
+    </div>
+  );
 };
 
 export function getStaticProps() {
+  console.log("Passando pelo getStaticProps");
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
 
